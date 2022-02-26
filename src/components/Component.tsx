051bd5db7e19b75d26140b9315470a61b0788382
Ye Seul Kim <yskim940914@gmail.com>
2022-02-27 01:32:08 +0900

    Add: 기본 컴포넌트 템플릿 생성

---
+++ src/components/Component.tsx
@@ -0,0 +1,12 @@
+import Pagination from 'base/Pagination';
+
+const Component = () => {
+  return (
+    <>
+      <div>컴포넌트1</div>
+      <Pagination />
+    </>
+  );
+};
+
+export default Component;
