e9770561c9f5b0cc68847b2d56ef4de271c48ae0
Ye Seul Kim <yskim940914@gmail.com>
2022-02-26 20:51:06 +0900

    Chore: 프로젝트 초기 설정

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    //.prettierrc.js 파일에서 설정
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Routes, Route, Link } from 'react-router-dom';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">dddd</header>
+      {/* <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="components" element={<Components />}>
+          <Route path=":componentsId" element={<Component />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes> */}
+    </div>
+  );
+}
+
+export default App;
